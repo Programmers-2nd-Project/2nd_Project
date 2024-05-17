e55238c873f1e2e40751bbbaac6cf503073749a7
dooby99 <102219869+dooby99@users.noreply.github.com>
2024-05-17 13:11:09 +0900

    Update LowestPrice_data.py

--- load/LowestPrice_data.py
+++ load/LowestPrice_data.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import os
 import requests
 import xmltodict
 import tkinter as tk
@@ -11,14 +12,13 @@ import xml.etree.ElementTree as ET
 import schedule
 import time
 import boto3
+import snowflake.connector
 
 def main():
-    print(2)
     Key = '******'
 
     # prodcd : 제품구분(휘발유:B027, 경유:D047, 고급휘발유: B034, 실내등유:C004, 자동차부탄: K015)
     # area : 지역구분 (미입력시 전국, 시도코드(2자리):해당시도 기준, 시군코드(4자리):해당시군 기준)
-    # 지역 코드 : http://www.opinet.co.kr/api/areaCode.do?out=xml&code=XXXXXX&area=01
 
     ## 지역코드
     url = f'http://www.opinet.co.kr/api/areaCode.do?out=xml&code={Key}'
@@ -75,22 +75,69 @@ def main():
     else:
         print(f'Failed to upload {file_name} to {bucket_name}.')
 
+    # snowflake 연결
+    conn = snowflake.connector.connect(
+    user='******', # snowflake ID
+    password='******', # snowflake password
+    account='******.ap-northeast-2.aws', # snowflake Locator ex) XQB*****
+    warehouse='COMPUTE_WH',
+    database='PROJ',
+    schema='RAW_DATA'
+    )
 
-# 매일 정해진 시간에 작업을 실행하도록 스케줄링
-print(1)
-schedule.every().day.at("09:00").do(main)
+    # SQL 쿼리 실행
+    try:
+        cursor = conn.cursor()
+        
+        # 테이블 생성 쿼리 실행
+        cursor.execute("""
+            CREATE OR REPLACE TABLE PROJ.RAW_DATA.LOWESTPRICE (
+            UNI_ID varchar(32) primary key,
+            PRICE integer,
+            POLL_DIV_CO varchar(32),
+            OS_NM varchar(32),
+            VAN_ADR varchar(32),
+            NEW_ADR varchar(32),
+            GIS_X_COOR float,
+            GIS_Y_COOR float,
+            AREA_NM varchar(32)
+            )
+        """)
+        
+        # S3 데이터를 테이블에 복사하는 쿼리 실행
+        cursor.execute("""
+            COPY INTO RAW_DATA.LOWESTPRICE
+            FROM 's3://{bucket}/{folder}/{file}'
+            credentials=(
+                AWS_KEY_ID='******' 
+                AWS_SECRET_KEY='******'
+            )
+            FILE_FORMAT = (type='CSV' skip_header=1 FIELD_OPTIONALLY_ENCLOSED_BY='"')
+        """)
+        
+        cursor.close()
+        
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        conn.close()
+    print("Snowflake Process Complete")
+
 # ---------------------------------------------------------------------------------------------------------------
 # S3 업로드 하는 코드
-# 터미널에서 aws configure 실행 후 s3 버킷 권한 있는 aws 사용자의 액세스 키, 시크릿 키, 리전 입력하고 사용
-# 연결됐는지 확인하려면 터미널에서 >> aws s3 ls 했을 때 자신의 버킷이 나와야 함
-# 이후 코드 실행하면 s3로 저장됨
 def upload_csv_to_s3(file_name, bucket_name, object_name=None):
     """Upload a CSV file to an S3 bucket."""
+    
+    # AWS 계정 정보 및 S3 버킷 정보 설정
+    aws_access_key_id = '*******'
+    aws_secret_access_key = '******'
+    bucket_name = '******'
+    
+    s3_client = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+
+    
     if object_name is None:
         object_name = os.path.basename(file_name)  # 파일 이름을 기반으로 객체 키 생성
-    
-    # Create an S3 client
-    s3_client = boto3.client('s3')
     
     try:
         # Upload the file
@@ -100,6 +147,11 @@ def upload_csv_to_s3(file_name, bucket_name, object_name=None):
         return False
     return True
 
+# 매일 정해진 시간에 작업을 실행하도록 스케줄링
+schedule.every().day.at("09:00").do(main)
+
+
+# 스케줄러 실행
 while True:
     schedule.run_pending()
     time.sleep(1)
